1af4e41de64f2684c7a7d64677d3ab6cee44b114
PiumiOshani <piumioshani@gmail.com>
2025-07-01 01:41:41 +0530

    added BookEvent file

--- Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/entity/BookEvent.java
+++ Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/entity/BookEvent.java
@@ -1,5 +1,49 @@
 package com.eventsync.Event.Sync.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.eventsync.Event.Sync.enums.BookingStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "book_events")
+
 public class BookEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int quantity;
+    private BigDecimal price;
+    private BookingStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "purchase_id")
+    private Purchase purchase;
+
+
+
+
+    @Column(name = "created_at")
+    private final LocalDateTime createdAt = LocalDateTime.now();
     
 }
