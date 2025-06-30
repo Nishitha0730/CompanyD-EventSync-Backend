cb6470b09c06e22a88ca7bb0518cc303aadbf203
Chamath Diunugala <157124897+chamathdiunugala@users.noreply.github.com>
2025-06-30 20:34:01 +0530

    added bookEventRepo file

--- Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/repository/BookEventRepo.java
+++ Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/repository/BookEventRepo.java
@@ -1,5 +1,12 @@
 package com.eventsync.Event.Sync.repository;
 
-public class BookEventRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import com.eventsync.Event.Sync.entity.BookEvent;
+
+public interface BookEventRepo extends JpaRepository<BookEvent, Long>, JpaSpecificationExecutor<BookEvent> {
+    
+    // Custom query methods can be defined here if needed
+    // For example, find by event name or date, etc.
     
 }
