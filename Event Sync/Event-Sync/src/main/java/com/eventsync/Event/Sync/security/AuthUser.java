a511babe21ffef0ef9bdd3e7edfda63ec1b4fd97
Agra Weragoda <weragodawaap.22@uom.lk>
2025-07-02 20:59:35 +0530

    AuthUser File Changed

--- Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/security/AuthUser.java
+++ Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/security/AuthUser.java
@@ -1,5 +1,50 @@
-package com.eventsync.Event.Sync.security;
+package com.eventsync.event.sync.security;
 
-public class AuthUser {
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.eventsync.event.sync.entity.User;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AuthUser implements UserDetails {
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
     
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isEnabled();
+    }
+    @Override
+    public boolean isEnabled() {
+        return true; // or your custom logic
+    }
 }
