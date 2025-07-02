e2d70865c8df1da459cca0bb47755d3d4f425328
Nishitha0730 <pitigalapknw.22@uom.lk>
2025-07-02 23:10:39 +0530

    LoginRequest.java updated

--- Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/dto/LoginRequest.java
+++ Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/dto/LoginRequest.java
@@ -1,5 +1,25 @@
 package com.eventsync.Event.Sync.dto;
 
 public class LoginRequest {
-    
+    private String username;
+    private String password;
+
+    // Getters & setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+
