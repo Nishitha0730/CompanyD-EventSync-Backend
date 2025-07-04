fa1321f7732851bd204480fb8bbfcc9de90c8f7d
Pirathishanth <arul.pirathi2002@gmail.com>
2025-07-04 18:15:16 +0530

    Add files via upload

--- Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/dto/EventDto.java
+++ Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/dto/EventDto.java
@@ -1,5 +1,27 @@
-package com.eventsync.Event.Sync.dto;
+package com.eventsync.event.sync.dto;
 
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class EventDto {
+    private long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String imageUrl;
+    private CategoryDto category;
+
+    
     
 }
