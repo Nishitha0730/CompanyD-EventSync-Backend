0a51c14f81181756d5a0ab599a3b73f147c56d97
Pasidu Nethmina <mapasidun@gmail.com>
2025-07-04 20:22:21 +0530

    Update Category.java
    updated file

--- Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/entity/Category.java
+++ Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/entity/Category.java
@@ -1,5 +1,36 @@
-package com.eventsync.Event.Sync.entity;
+package com.eventsync.event.sync.entity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Event> events;
+
+    @Column(name = "created_at")
+    private final LocalDateTime createdAt = LocalDateTime.now();
+
+
     
 }
