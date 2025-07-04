e9f15f194fac5424deb484dc9d4e3f3dfac7709b
Pasidu Nethmina <mapasidun@gmail.com>
2025-07-04 20:23:37 +0530

    Update Event.java

--- Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/entity/Event.java
+++ Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/entity/Event.java
@@ -1,5 +1,45 @@
-package com.eventsync.Event.Sync.entity;
+package com.eventsync.event.sync.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "events")
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    //private LocalDateTime eventDate;
+
+    private String imageUrl;
+    private String description;
+    //
+    private BigDecimal price;
+    private String location;
+    private String startTime;
+    private String endTime;
+    private String date;
+    //
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(name = "created_at")
+    private final LocalDateTime createdAt = LocalDateTime.now();
     
 }
