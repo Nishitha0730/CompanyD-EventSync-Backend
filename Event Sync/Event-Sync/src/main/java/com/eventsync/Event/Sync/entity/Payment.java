0f6d495f385b0a90eee92df27ad9ea9311e75301
Pasidu Nethmina <mapasidun@gmail.com>
2025-07-04 20:24:28 +0530

    Update Payment.java
    updated file

--- Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/entity/Payment.java
+++ Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/entity/Payment.java
@@ -1,5 +1,36 @@
-package com.eventsync.Event.Sync.entity;
+package com.eventsync.event.sync.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "payments")
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+    private String method;
+    private String status;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "purchase_id")
+    private Purchase purchase;
+    @Column(name = "created_at")
+    private final LocalDateTime createdAt = LocalDateTime.now();
     
 }
