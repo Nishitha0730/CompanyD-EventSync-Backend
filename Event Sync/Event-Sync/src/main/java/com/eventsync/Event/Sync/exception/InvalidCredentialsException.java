c90c850a7f2ebee5f9f44f9d2f3c56b5137456dd
Rakindu-L <rakindu.paranayapa@gmail.comm>
2025-07-04 17:09:01 +0530

    invalid credentials exception and not found exception classes added

--- Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/exception/InvalidCredentialsException.java
+++ Event Sync/Event-Sync/src/main/java/com/eventsync/Event/Sync/exception/InvalidCredentialsException.java
@@ -1,7 +1,10 @@
 package com.eventsync.Event.Sync.exception;
 
 public class InvalidCredentialsException extends RuntimeException {
+
     public InvalidCredentialsException(String message) {
         super(message);
     }
+
+    
 }
